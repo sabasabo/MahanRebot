c893e362ea19da136774fe4d3d9df4251668391b
sabasabo <lir48290@gmail.com>
2018-07-09 23:59:23 +0300

    add easy runBot linux

--- runBot.sh
+++ runBot.sh
@@ -1,7 +1,30 @@
 git reset --hard
 git pull
 npm install
-token=`tail -1 token.txt`
-sed -i.bak 's/TOKEN/$token/g' app.js
+sed -i.bak "s/TOKEN/`tail -1 token.txt`/g" app.js
 rm app.js.bak
-npm start &
+npm start
+
+
+
+# npm start &
+# process=`echo $!`
+# echo "killing a"
+# kill -p $process
+
+
+##!/bin/bash
+#
+#while true; do
+#	if [ git remote update && git status -uno ]; then
+#		
+#		git reset --hard
+#		git pull
+#		npm install
+#	
+#		token=`tail -1 token.txt`
+#		sed -i.bak 's/TOKEN/$token/g' app.js
+#		npm start &
+#	fi
+#	sleep 300
+#done
